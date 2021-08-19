8e7565d3bd90b3bd370bfde39990292403c1cda2
Evgeny <daem0nbfg@gmail.com>
2021-08-19 16:48:25 +0300

    OAuth Params

--- src/hooks/OAuth.ts
+++ src/hooks/OAuth.ts
@@ -1,11 +1,21 @@
-export default abstract class OAuth {
-  readonly oAuthClientURL: string
+export interface IOAuthParams {
+  client_id: string
+  redirect_uri: string
+  response_type: "token"
+  scope?: string
+  include_granted_scopes?: boolean
+  state?: string
+}
 
-  readonly oAuthClientId: string
+export default abstract class OAuth {
+  readonly oAuthParams: IOAuthParams
 
   constructor(serverURL: string, clientId: string) {
-    this.oAuthClientURL = serverURL
-    this.oAuthClientId = clientId
+    this.oAuthParams = {
+      redirect_uri: serverURL,
+      client_id: clientId,
+      response_type: "token",
+    }
   }
 
   abstract getRedirectURL(): string
--- src/hooks/YandexOAuth.ts
+++ src/hooks/YandexOAuth.ts
@@ -1,12 +1,16 @@
-import OAuth from "./OAuth"
+import OAuth, { IOAuthParams } from "./OAuth"
 
 export default class YandexOAuth extends OAuth {
   getRedirectURL(): string {
-    return `https://oauth.yandex.ru/authorize?response_type=token&client_id=${this.oAuthClientId}&redirect_uri=${this.oAuthClientURL}`
+    return Object.keys(this.oAuthParams).reduce((acc, item, index, array) => {
+      return `${acc}${item}=${this.oAuthParams[item as keyof IOAuthParams]}${
+        index + 1 < array.length ? "&" : ""
+      }`
+    }, "https://oauth.yandex.ru/authorize?")
   }
 
   // eslint-disable-next-line class-methods-use-this
-  extractToken(urlWithToken: string): any {
+  extractToken(urlWithToken: string): string {
     return urlWithToken.split(
       /^#access_token=(\S*)&token_type=(\S*)&expires_in=(\d*)/gi
     )[1]
